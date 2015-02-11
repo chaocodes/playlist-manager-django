c4985bfec5d075c9982097663471d0997e9a38f0
Chao Lai <chao.lai@uconn.edu>
2015-02-10 20:56:15 -0500

    Check if manager/local.py exists before importing

--- manager/settings.py
+++ manager/settings.py
@@ -1,6 +1,20 @@
 from django.contrib import messages
 import os
-from local import *
+try:
+    from local import *
+except ImportError:
+    BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+    SECRET_KEY = 'SECRET_KEY'
+    DEBUG = True
+    ADMINS = (
+        ('Name', 'name@gmail.com'),
+    )
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 TEMPLATE_DEBUG = DEBUG
 MANAGERS = ADMINS
